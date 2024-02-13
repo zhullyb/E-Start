dcc0a0743888aff394baa3e806cc7cf4dfd267b6
zhullyb <zhullyb@outlook.com>
2024-02-13 19:08:22 +0800

    set center for login page

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -20,47 +20,59 @@ const login = () => {
 </script>
 
 <template>
-    <view
-        style="text-align: center;"
-    >
-        <image
-            src="../../static/logo.png"
-            mode="widthFix"
-            style="width: 75%;"
-        />
+    <view class="center">
+        <view
+            style="text-align: center;"
+        >
+            <image
+                src="../../static/logo.png"
+                mode="widthFix"
+                style="width: 75%;"
+            />
+        </view>
+    
+        <view
+            style="text-align: center;"
+        >
+            <text
+                style="font-size: xx-large; color: white;"
+            >E启新篇</text>
+        </view>
+    
+        <button
+            :disabled="false"
+            :loading="false"
+            open-type=""
+            hover-class="button-hover"
+            @click="login"
+            style="
+                width: 70vw;
+                margin-top: 20vh;
+                border-radius: 16px;
+                border: 1px solid rgb(63, 114, 175);
+                padding: 8px;
+                color: rgb(17, 45, 78);
+                font-weight: bold;
+                margin-bottom: 10vh;
+            "
+        >
+            一键授权登陆
+        </button>
     </view>
-
-    <view
-        style="text-align: center;"
-    >
-        <text
-            style="font-size: xx-large; color: white;"
-        >E启新篇</text>
-    </view>
-
-    <button
-        :disabled="false"
-        :loading="false"
-        open-type=""
-        hover-class="button-hover"
-        @click="login"
-        style="
-            width: 70%;
-            margin-top: 20vh;
-            border-radius: 16px;
-            border: 1px solid rgb(63, 114, 175);
-            padding: 8px;
-            color: rgb(17, 45, 78);
-            font-weight: bold;
-        "
-    >
-        一键授权登陆
-    </button>
 </template>
 
 
 <style>
-    page {
-        background: url('../../static/bg-small.png') no-repeat;
-    }
+page {
+    background: url('../../static/bg-small.png') no-repeat;
+    position: relative;
+    height: 100%;
+}
+
+.center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
 </style>
