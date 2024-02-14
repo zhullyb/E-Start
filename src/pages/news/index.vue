2bfbda5cc9b5a23332e48920dae37821f3afeb22
zhullyb <zhullyb@outlook.com>
2024-02-15 06:17:05 +0800

    news: show messages now

--- src/pages/news/index.vue
+++ src/pages/news/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const selectedItem = ref(1);
 
@@ -9,19 +9,89 @@ const buttons = [
     { text: '系统公告' }
 ];
 
+const originInfoList = [
+    {   
+        id: 1,
+        title: '最新通知1',
+        content: '这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容'
+    },
+    {
+        id: 2,
+        title: '最新通知2',
+        content: '这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容'
+    },
+    {
+        id: 3,
+        title: '最新通知3',
+        content: '这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容'
+    },
+    {
+        id: 4,
+        title: '最新通知4',
+        content: '这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容'
+    },
+    {
+        id: 5,
+        title: '最新通知5',
+        content: '这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容'
+    },
+    {
+        id: 6,
+        title: '最新通知6',
+        content: '这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容'
+    },
+    {
+        id: 7,
+        title: '最新通知7',
+        content: '这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容'
+    }
+]
+
+const infoList = computed(
+    () => originInfoList.filter(
+        item => item.id % 3 === selectedItem.value
+    )
+)
 </script>
 
 <template>
-<uni-row
-    :gutter="12"
-    style="margin: 12px;"
->
-    <uni-col :span="8" v-for="(button, index) in buttons" :key="index">
-        <button
-            class="es-button"
-            :class="{ selected: selectedItem === index }"
-            @click="selectedItem = index"
-        >{{ button.text }}</button>
-    </uni-col>
-</uni-row>
+<view style="margin: 16px;">
+    <uni-row
+        :gutter="12"
+        style="margin-bottom: 20px;"
+    >
+        <uni-col :span="8" v-for="(button, index) in buttons" :key="index">
+            <button
+                class="es-button"
+                :class="{ selected: selectedItem === index }"
+                @click="selectedItem = index"
+            >{{ button.text }}</button>
+        </uni-col>
+    </uni-row>
+    <view>
+        <view 
+            v-for="item in infoList"
+            style="margin: 30px 0;"
+        >
+            <text
+                style="
+                        color: rgb(17, 45, 78);
+                        font-size: 17px;
+                        font-weight: 400;
+                        line-height: 23.8px;
+                    "
+            >{{ item.title }}</text>
+            <br />
+            <text
+                style="
+                        color: rgb(17, 45, 78);
+                        font-size: 14px;
+                        font-weight: 400;
+                        line-height: 19.6px;
+                        opacity: 0.5;
+                    "
+            >{{ item.content }}</text>
+        </view>
+    </view>
+</view>
 </template>
