652b299094778bf896c6bf946bbd3a3a2863f63f
zhullyb <zhullyb@outlook.com>
2024-02-15 04:33:45 +0800

    add news page

---
+++ src/pages/news/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const selectedItem = ref(1);
+
+</script>
+
+<template>
+<uni-row
+    :gutter="12"
+    style="margin: 12px;"
+>
+    <uni-col :span="8">
+        <button
+            class="es-button"
+            :class="{ selected: selectedItem === 0 }"
+            @click="selectedItem = 0"
+        >最新通知</button>
+    </uni-col>
+    <uni-col :span="8">
+        <button
+            class="es-button"
+            :class="{ selected: selectedItem === 1 }"
+            @click="selectedItem = 1"
+        >新闻动态</button>
+    </uni-col>
+    <uni-col :span="8">
+        <button
+            class="es-button"
+            :class="{ selected: selectedItem === 2 }"
+            @click="selectedItem = 2"
+        >系统公告</button>
+    </uni-col>
+</uni-row>
+</template>
