a92d85e4302f45b3893325fd9ed982c778130617
zhullyb <zhullyb@outlook.com>
2024-02-17 11:13:59 +0800

    news/detail: working now

--- src/pages/news/detail.vue
+++ src/pages/news/detail.vue
@@ -1,27 +1,18 @@
 <script setup lang="ts">
-// import { onLoad } from '@dcloudio/uni-app';
-// import { getCurrentInstance, ref } from 'vue';
+import { onLoad } from '@dcloudio/uni-app';
+import { getCurrentInstance, ref } from 'vue';
+import type { info } from '../../types/info';
 
-// const detail = ref();
-// const $instance = getCurrentInstance()?.proxy as any
-// onLoad(() => {
-//     const eventChannel = $instance.getOpenerEventChannel()
-//     eventChannel.on('acceptDataFromOpenerPage', (data: any) => {
-//         detail.value = data.data
-//         console.log(data.data)
-//     })
-// })
-
-const detail = {
-        "id": 3,
-        "createdAt": "2024-01-29",
-        "updatedAt": "2004-01-29",
-        "category": 14,
-        "title": "停网公告",
-        "source": "信息化办公室",
-        "content": "各位师生:\n根据学校《施工停电公告》（具体见https://www.zjut.edu.cn/2024/0129/c4528a254704/page.htm），2024年2月1日至2月28日期间，三校区在停电的相应时间段会出现局部或全局停网现象。信息化办公室将根据实际停电的时间，尽可能实时调整有关UPS和网络设备的运行，最大限度地保证关键应用系统和内部网络的正常运行。\n请各用户做好相应准备，停网给您带来的不便，敬请您理解。如有问题，请联系：88320094（朝晖）、85290091（屏峰）、88813091（莫干山）。",
-        "imgs": "http://dummyimage.com/400x400"
-    }
+const detail = ref<info>();
+const $instance = getCurrentInstance()?.proxy as any
+onLoad(() => {
+    const eventChannel = $instance.getOpenerEventChannel()
+    eventChannel.on('acceptDataFromOpenerPage', (data: {data: info}) => {
+        detail.value = data.data
+        const time = new Date(data.data.updatedAt)
+        detail.value.updatedAt = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`
+    })
+})
 
 </script>
 
@@ -35,7 +26,7 @@ const detail = {
                         font-weight: 400;
                         line-height: 23.8px;
                     "
-            >{{ detail.title }}</text>
+            >{{ detail?.title }}</text>
         </view>
         <view style="margin: 2px 0;">
             <text
@@ -45,7 +36,7 @@ const detail = {
                         font-weight: 400;
                         line-height: 20px;
                     "
-            >来源：{{ detail.source }}</text>
+            >来源：{{ detail?.source }}</text>
         </view>
         <view style="margin-bottom: 10px;">
             <text
@@ -55,7 +46,7 @@ const detail = {
                         font-weight: 400;
                         line-height: 20px;
                     "
-            >时间：{{ detail.updatedAt }}</text>
+            >时间：{{ detail?.updatedAt }}</text>
         </view>
         <view>
             <text
@@ -67,7 +58,7 @@ const detail = {
                         line-height: 25px;
                         word-wrap: break-word;
                     "
-            >{{ detail.content }}</text>
+            >{{ detail?.content }}</text>
         </view>
     </view>
 </template>
