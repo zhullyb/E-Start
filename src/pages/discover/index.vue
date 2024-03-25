c480c5f32bb41db2c604a1427a299b1f6adbffc0
zhullyb <zhullyb@outlook.com>
2024-03-25 23:22:29 +0800

    discover/index: add map redirect

--- src/pages/discover/index.vue
+++ src/pages/discover/index.vue
@@ -55,7 +55,7 @@ const clickItem = (index: number) => {
 
 </script>
 <template>
-    <view>
+    <view style="background-color: #F9FAFB;">
         <view class="l-swiper">
             <swiper autoplay circular class="swiper-box" style="--height: 300rpx" previous-margin="60rpx"
                 next-margin="60rpx" @change="change">
@@ -65,7 +65,7 @@ const clickItem = (index: number) => {
                 </swiper-item>
             </swiper>
         </view>
-        <view style="margin: 20px;">
+        <view style="margin: 16px 32px;">
             <uni-row :gutter="16">
                 <uni-col :span="6" v-for="item in iconInfo">
                     <view style="text-align: center; margin: 8px;">
@@ -81,6 +81,34 @@ const clickItem = (index: number) => {
                 </uni-col>
             </uni-row>
         </view>
+        <view style="margin: 10px;">
+            <view style="border-radius: 16px; background-color: #FFFFFF;">
+                <uni-row>
+                    <view style="display: flex; align-items: center;">
+                        <uni-col :span="6">
+                            <view style="margin:10px; background-color: #FFF1DE; border-radius: 16px; width: min-content;">
+                                <image
+                                    src="/static/discover/map-icon.png"
+                                    mode="scaleToFill"
+                                    style="width: 40px; height: 36px; margin: 10px;"
+                                />
+                            </view>
+                        </uni-col>
+                        <uni-col :span="16">
+                            <view style="font-size: 16px; font-weight: bold;">点击查看校园地图</view>
+                            <view style="font-size: 14px; color: #999999;">还可进行校园导航</view>
+                        </uni-col>
+                        <uni-col :span="2">
+                            <image
+                                src="/static/discover/right-arrow.png"
+                                mode="scaleToFill"
+                                style="height: 12px; width: 12px;"
+                            />
+                        </uni-col>
+                    </view>
+                </uni-row>
+            </view>
+        </view>
     </view>
 </template>
 
@@ -89,7 +117,6 @@ const clickItem = (index: number) => {
     width: 100%;
     padding: 20rpx 0;
     box-sizing: border-box;
-    background: #fff;
 
     .swiper-box {
         width: 100%;
