426630d38f74aa02b914204111cd9d40c5b32b0e
zhullyb <zhullyb@outlook.com>
2024-03-26 14:52:41 +0800

    mine/index: use computed for score

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
+const score = 600
+
 const images = {
     "v1-text": "https://bu.dusays.com/2024/03/26/66023e26ee444.png",
     "v2-text": "https://bu.dusays.com/2024/03/26/66023ef3090f6.png",
@@ -11,6 +15,26 @@ const images = {
     "v4-figure": "https://bu.dusays.com/2024/03/26/66023e1a418b7.png",
     "v5-figure": "https://bu.dusays.com/2024/03/26/66023e1a63757.png" 
 }
+
+const levelInfo = computed(
+    () => {
+        const level = [
+            { min: 0, max: 100, imgText: images["v1-text"], imgFigure: images["v1-figure"] },
+            { min: 100, max: 500, imgText: images["v2-text"], imgFigure: images["v2-figure"] },
+            { min: 500, max: 1000, imgText: images["v3-text"], imgFigure: images["v3-figure"] },
+            { min: 1000, max: 2000, imgText: images["v4-text"], imgFigure: images["v4-figure"] },
+            { min: 2000, imgText: images["v5-text"], imgFigure: images["v5-figure"] },
+        ]
+        for (const l of level) {
+            if (score >= l.min && score < (l.max || Infinity)) {
+                const need = (l.max || Infinity) - score
+                const percent = (score - l.min) / ((l.max || 2000) - l.min)
+                return { level: level.indexOf(l) + 1, need, percent, imgText: l.imgText, imgFigure: l.imgFigure }
+            }
+        }
+        return { level: 1, need: 100, percent: 0, imgText: images["v1-text"], imgFigure: images["v1-figure"] }
+    }
+)
 </script>
 
 <template>
@@ -41,33 +65,33 @@ const images = {
                 <uni-col :span="12">
                     <view style="margin: 20px;">
                         <image
-                            :src="images['v1-text']"
+                            :src="levelInfo.imgText"
                             mode="heightFix"
                             style="height: 36px;"
                         />
-                        <view style="color: #977A48; font-size: 15px; margin: 4px 0;">智慧值: 55</view>
-                        <view style="color: #977A48; font-size: 12px; margin: 4px 0;">还需要 45 点智慧值升级</view>
+                        <view style="color: #977A48; font-size: 15px; margin: 4px 0; font-weight: bold;">智慧值: {{ score }}</view>
+                        <view style="color: #977A48; font-size: 12px; margin: 4px 0;">还需要 {{ levelInfo.need }} 点智慧值升级</view>
                         <view style="margin-top: 20px; height: 8px; background-color: rgb(192, 198, 200); border-radius: 8px;">
                             <view
                                 style="
                                     height: 8px;
                                     background: linear-gradient(90.00deg, rgb(225, 202, 164) 17.899%,rgb(181, 151, 101) 88.204%);
                                     border-radius: 8px;"
-                                :style="{ width: '55%' }"
+                                :style="{ width: `${levelInfo.percent * 100}%` }"
                             ></view>
                         </view>
                         <view style="display: flex; margin-top: 8px;">
                             <text style="margin: auto auto auto 0;
-                                            color: #977A48; font-size: 12px;">V1</text>
+                                            color: #977A48; font-size: 12px;">V{{ levelInfo.level }}</text>
                             <text style="margin: auto 0 auto auto;
-                                            color: #977A48; font-size: 12px;">v2</text>
+                                            color: #977A48; font-size: 12px;">v{{ levelInfo.level+1 }}</text>
                         </view>
                     </view>
                 </uni-col>
                 <uni-col :span="12">
                     <view style="margin: -5px;">
                         <image
-                            :src="images['v1-figure']"
+                            :src="levelInfo.imgFigure"
                             mode="widthFix"
                             style="width: 100%;"
                         />
@@ -82,6 +106,7 @@ const images = {
                 </uni-col>
             </uni-row>
         </view>
+        <view style="font-size: 20px; margin: 16px;">账户信息</view>
     </view>
 </template>
 
