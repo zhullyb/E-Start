5a97eee10079792e3694970dbfc9f0dcfe1f5e26
zhullyb <zhullyb@outlook.com>
2024-04-13 17:27:11 +0800

    index/index: outlook bring up

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,28 +1,15 @@
-<template>
-    <view class="content">
-        <image class="logo" src="/static/logo.png" @click="toNews" />
-        <view class="text-area">
-            <text class="title">{{ title }}</text>
-        </view>
-    </view>
-    <view style="display: none;">
-        <image
-            v-for="image in imageToPreload"
-            :src="image"
-            mode="scaleToFill"
-        />
-    </view>
-</template>
-
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { ref } from 'vue'
 const title = ref('Hello')
 
-const toNews = () => {
-    uni.navigateTo({ url: '/pages/tasks/buy' })
+const toNewsList = () => {
+    uni.navigateTo({ url: '/pages/news/index' })
 }
 
+const toTasks = () => {
+    uni.switchTab({ url: '/pages/tasks/index' })
+}
 
 const imageToPreload = [
     "https://bu.dusays.com/2024/03/27/66030a1c8ae3d.webp",
@@ -49,30 +36,214 @@ onMounted(() => {
 })
 </script>
 
-<style>
-.content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+<template>
+    <view>
+        <view id="index-board">
+            <view style="height: 44px; background: transparent;"></view>
+            <view style="color: #FFFFFF; font-size: 16px; margin: 8px 20px;">E启新篇</view>
+            <uni-row>
+                <uni-col :span="14">
+                    <view style="margin: 16px 16px;">
+                        <view class="title">童博扬</view>
+                        <view class="title" style="font-size: 16px;">(Lv.1 启程者)</view>
+                    </view>
+                    <view style="margin-left: 20px; margin-top: 12px;">
+                        <text class="tag">50 智慧值</text>
+                        <text class="tag">50 求索值</text>
+                    </view>
+                </uni-col>
+                <uni-col :span="10">
+                    <image
+                        src="https://bu.dusays.com/2024/03/27/66030a1ccdeb5.webp"
+                        mode="widthFix"
+                        style="width: 120%; margin-left: -30px;"
+                    />
+                </uni-col>
+            </uni-row>
+            <view>
+                <text style="margin-left: 20px; font-size: 16px; color: #FFFFFF;">主线任务进度: </text>
+                <text style="margin-left: 4px; font-size: 16px; color: #FFFFFF; font-weight: bolder;">8%</text>
+                <view style="margin: 20px;">
+                    <view style="height: 6px; width: 100%; background: rgba(3, 3, 3, 0.4); border-radius: 6px;">
+                        <view style="height: 6px; width: 8%; background: #1FE032; border-radius: 6px;"></view>
+                    </view>
+                </view>
+            </view>
+            <view style="height: 8px;"></view>
+        </view>
+        <view>
+            <view style="display: flex;">
+                <view class="title">新闻公告</view>
+                <view class="check-more" @click="toNewsList">查看更多</view>
+            </view>
+            <view class="white-board">
+                <view class="news-title">国家板球集训队在我校开展冬季集训</view>
+                <view class="news-text">1月中旬起，国家板球队入驻浙江工业大学（屏峰校区）板球场开展冬季集训，备战亚洲板球理事会男子T20挑战者杯比赛和亚洲板球理事会女子板球精英赛。</view>
+            </view>
+        </view>
+        <view style="text-align: center; color: #3F72AF; margin: 12px 0;">家庭有困难？点击申请绿色通道</view>
+        <view>
+            <view style="display: flex;">
+                <view class="title">在线咨询</view>
+                <view class="check-more">人工服务</view>
+            </view>
+            <view id="chat-search">
+                <uni-icons type="search" size="24" style="margin: auto 0;"/>
+                <text style="margin: auto 8px; color: #999999; font-size: 14px;">
+                    AI智能在线咨询
+                </text>
+            </view>
+        </view>
+        <view>
+            <view style="display: flex;">
+                <view class="title">待办任务</view>
+                <view class="check-more" @click="toTasks">查看更多</view>
+            </view>
+            <scroll-view scroll-x style="margin: 12px 15px; white-space: nowrap;">
+                <view class="task-card" v-for="i in [0, 1, 2]">
+                    <view class="task-title">准备脸盆</view>
+                    <view style="display: flex;">
+                        <text class="task-text">截止时间</text>
+                        <text class="task-text bold" style="margin: auto 0px auto auto;">2024年9月1日12:00</text>
+                    </view>
+                    <view style="display: flex;">
+                        <text class="task-text">任务进度</text>
+                        <text class="task-text bold" style="margin: auto 0px auto auto;">50%</text>
+                    </view>
+                    <view style="margin-top: 12px;">
+                        <view style="height: 4px; width: 100%; background: rgba(3, 3, 3, 0.4); border-radius: 4px;">
+                            <view style="height: 4px; width: 50%; background: #3F72AF; border-radius: 4px;"></view>
+                        </view>
+                    </view>
+                </view>
+            </scroll-view>
+        </view>
+        <view style="height: 16px;"></view>
+    </view>
+    <!-- 图片预加载 -->
+    <view style="display: none;">
+        <image
+            v-for="image in imageToPreload"
+            :src="image"
+            mode="scaleToFill"
+        />
+    </view>
+</template>
+
+<style scoped>
+#index-board {
+    width: 100%;
+    background: linear-gradient(135deg, #5563DC, #5EC1F5);
+    border-radius: 0px 0px 32px 32px;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    margin-bottom: 20px;
 }
 
-.logo {
-    height: 200rpx;
-    width: 200rpx;
-    margin-top: 200rpx;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 50rpx;
+#index-board .title {
+    color: #fff;
+    font-size: 20px;
+    font-weight: 500;
 }
 
-.text-area {
-    display: flex;
-    justify-content: center;
+#index-board .tag {
+    margin-right: 10px;
+    margin-bottom: 8px;
+    border: 1px solid #E4EAF1;
+    padding: 4px;
+    width: 60px;
+    text-align: center;
+    border-radius: 4px;
+    color: #667085;
+    font-size: 12px;
+    background: #fff;
 }
 
 .title {
-    font-size: 36rpx;
-    color: #8f8f94;
+    font-size: 20px;
+    color: #112D4E;
+    font-weight: 500;
+    margin-left: 20px;
+}
+
+.white-board {
+    background-color: #fff;
+    padding: 12px 20px;
+    margin: 6px 0px;
+}
+
+.news-title {
+    color: #112D4E;
+    font-size: 17px;
+    font-weight: 400;
+    line-height: 23.8px;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    overflow: hidden;
+    word-break: break-all;
+    margin-bottom: 6px;
+}
+
+.news-text {
+    color: #112D4E;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 19.6px;
+    opacity: 0.5;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    overflow: hidden;
+    word-break: break-all;
+}
+
+.check-more {
+    margin: auto 20px auto auto;
+    color: #3F72AF;
+    font-size: 14px;
+    font-weight: 400;
+}
+
+#chat-search {
+    background-color: #FFFFFF;
+    height: 60px;
+    margin: 12px 20px;
+    border-radius: 50px;
+    display: flex;
+    border: 1px solid rgb(152, 162, 179);
+    padding: 0px 16px;
+}
+
+.task-card {
+    background-color: #fff;
+    border-radius: 12px;
+    border: 1px dashed #3F72AF;
+    padding: 12px;
+    width: 200px;
+    display: inline-block;
+    margin-right: 10px;
+}
+
+.task-title {
+    color: #3F72AF;
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 4px;
+}
+
+.task-text {
+    color: #667085;
+    font-size: 14px;
+    font-weight: 400;
+}
+
+.task-text.bold {
+    font-weight: 700;
+}
+</style>
+
+<style>
+page {
+    background-color: #F9FAFB;
 }
 </style>
